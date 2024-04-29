6ebaf5ecc9e12d37331e354f99a82ec3e3dfe5b6
farioraro <carlos8a29@hotmail.com>
2024-04-29 18:07:11 -0500

    Se añadio Json Server

--- src/app/public/pages/repos/repos.component.ts
+++ src/app/public/pages/repos/repos.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
-  styleUrl: './repos.component.css'
+  styleUrls: ['./repos.component.css']
 })
-export class ReposComponent {
 
+export class ReposComponent implements OnInit {
+  repos: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('/api/repositories').subscribe((data: any) => {
+      this.repos = data;
+    });
+  }
 }
