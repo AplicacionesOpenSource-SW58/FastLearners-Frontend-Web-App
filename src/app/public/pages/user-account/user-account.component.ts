b84e22e5e71c8d1d973ceab257d215765aefda84
Mathias Javier Murillo <115802009+K1ngHulk@users.noreply.github.com>
2024-04-29 11:25:02 -0500

    Update user-account.component.ts

--- src/app/public/pages/user-account/user-account.component.ts
+++ src/app/public/pages/user-account/user-account.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-account',
   templateUrl: './user-account.component.html',
-  styleUrl: './user-account.component.css'
+  styleUrls: ['./user-account.component.css']
 })
 export class UserAccountComponent {
+  user = {
+    username: 'exampleUser',
+    email: 'example@example.com',
+  };
 
+  constructor() {}
 }
