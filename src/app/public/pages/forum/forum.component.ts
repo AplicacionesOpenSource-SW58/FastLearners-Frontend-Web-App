973cb8a206ebe08098a208835e2d41bc96b8b376
farioraro <carlos8a29@hotmail.com>
2024-06-02 03:50:27 -0500

    chore: improve styles

--- src/app/public/pages/forum/forum.component.ts
+++ src/app/public/pages/forum/forum.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-forum',
   templateUrl: './forum.component.html',
   styleUrl: './forum.component.css'
 })
-export class ForumComponent {
 
+export class ForumComponent implements OnInit {
+  forums: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/forums').subscribe((data: any) => {
+      this.forums = data;
+    });
+  }
 }
