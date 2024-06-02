e01056c0f4498bd2b469f688aa6be63a299d5597
farioraro <carlos8a29@hotmail.com>
2024-06-02 18:47:02 -0500

    chore: added channels

--- src/app/public/pages/multimedia/multimedia.component.ts
+++ src/app/public/pages/multimedia/multimedia.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-multimedia',
   templateUrl: './multimedia.component.html',
   styleUrl: './multimedia.component.css'
 })
-export class MultimediaComponent {
 
+export class MultimediaComponent implements OnInit{
+  channels: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:3000/channels').subscribe((data: any) => {
+      this.channels = data;
+    });
+  }
 }
